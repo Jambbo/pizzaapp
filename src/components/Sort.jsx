279bf9aae5b58946d09ec635a1338d97939d81a4
Gleb <chibgleb99@gmail.com>
2025-07-28 14:11:06 +0100

    implemented closing sort tab

--- src/components/Sort.jsx
+++ src/components/Sort.jsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react'
+import React, {useEffect, useRef, useState} from 'react'
 import {useSelector, useDispatch} from "react-redux";
 import {setSortType} from '../redux/slices/filterSlice'
 
@@ -21,6 +21,7 @@ export function Sort() {
     const sort = useSelector((state) => state.filter.sortType);
     const dispatch = useDispatch();
     const [open, setOpen] = useState(false);
+    const sortRef = useRef();
 
 
     const onClickListItem = (sortType) => {
@@ -28,8 +29,20 @@ export function Sort() {
         setOpen(prevState => !prevState)
     };
 
+    useEffect(() => {
+        const handleClickOutside = (event) => {
+            if (!event.composedPath().includes(sortRef.current)) {
+                setOpen(false);
+            }
+        }
+        document.body.addEventListener('click', handleClickOutside);
+
+        return () => document.body.removeEventListener('click', handleClickOutside);
+
+    }, []);
+
     return (
-        <div className="sort">
+        <div ref={sortRef} className="sort">
             <div className="sort__label">
                 <svg
                     width="10"
